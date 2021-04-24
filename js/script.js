14c721e10f61e509d59ea51f44e60dd976328d6e
AzazelMaquiavelo <daniel.d.boscan@gmail.com>
2021-04-24 00:31:45 -0500

    header, slider.succes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 $(window).on('load', function() {
     $('#slider').nivoSlider(); 
 });
+
+//mostrar y ocultar menu
+
+const btnMn = document.querySelector('.btn-menu');
+
+btnMn.addEventListener('click', mostrarMenu);
+
+function mostrarMenu(e) {
+    e.preventDefault();
+
+    const menu = document.querySelector('.lista');
+
+    if(menu.classList.contains('mostrar')) {
+        //remover la clase mostrar
+        menu.classList.remove('mostrar');
+    
+    } else {
+        //mostrar el menu
+        menu.classList.add('mostrar');
+
+    }
+}
