dafd8e280cf1ada1cdea429a9fce9fff35313471
AzazelMaquiavelo <daniel.d.boscan@gmail.com>
2021-06-08 03:54:08 -0500

    modalFinish

--- js/script.js
+++ js/script.js
@@ -1,25 +1,64 @@
+//variables
+const btnMn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.lista');
+const cerrarModal = document.querySelector('.close');
+const abrirModal = document.querySelector('.cta');
+const modal = document.querySelector('.modal');
+const modalCont = document.querySelector('.modal-container');
+
+eventListeners();
+function eventListeners() {
+    btnMn.addEventListener('click', mostrarMenu);
+    abrirModal.addEventListener('click', mostrarModal);
+    cerrarModal.addEventListener('click', ocultarModal);
+    window.addEventListener('click', (e) => {
+        console.log(e.target);
+        if(e.target == modalCont) {
+            modal.classList.toggle("modal-close");
+            setTimeout(() => {
+                modalCont.style.opacity = "0";
+                modalCont.style.visibility = "hidden";
+            }, 900);
+        }
+    });
+}
+
+//funciones
+
+//nivoSlider
 $(window).on('load', function() {
     $('#slider').nivoSlider(); 
 });
 
 //mostrar y ocultar menu
-
-const btnMn = document.querySelector('.btn-menu');
-
-btnMn.addEventListener('click', mostrarMenu);
-
 function mostrarMenu(e) {
     e.preventDefault();
-
-    const menu = document.querySelector('.lista');
-
-    if(menu.classList.contains('mostrar')) {
-        //remover la clase mostrar
-        menu.classList.remove('mostrar');
+    menu.classList.toggle("lista-close");
     
+    if(menu.classList.contains('lista-close')) {
+        setTimeout(() => {
+            menu.style.opacity = "0";
+            menu.style.visibility = "hidden";
+        },900);
     } else {
-        //mostrar el menu
-        menu.classList.add('mostrar');
-
+        menu.style.opacity = "1";
+        menu.style.visibility = "visible";
     }
+}
+
+//mostrar y ocultar modal
+function mostrarModal(e) {
+    e.preventDefault();
+    modalCont.style.opacity = "1";
+    modalCont.style.visibility = "visible";
+    modal.classList.toggle("modal-close");
+
+}
+function ocultarModal() {
+    
+    modal.classList.toggle("modal-close");
+    setTimeout(() => {
+        modalCont.style.opacity = "0";
+        modalCont.style.visibility = "hidden";
+    }, 900);
 }
