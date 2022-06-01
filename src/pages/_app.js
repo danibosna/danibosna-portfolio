e0622da8af184c0a340098366fc81dc9866d6d8e
danibosna <daniel.d.boscan@gmail.com>
2022-06-01 13:52:23 -0500

    [FIX]google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,23 +16,14 @@ function MyApp({ Component, pageProps, titleHead, descriptionHead }) {
         <title>{titleHead}</title>
         <meta name="description" content={descriptionHead} />
       </Head>
-<<<<<<< HEAD
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T04GQRL8VP"></Script>
-      <Script id="google-analytics">
-=======
       <Script strategy="afterInteractive" async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
       <Script strategy="afterInteractive" id="google-analytics">
->>>>>>> dev
         {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-<<<<<<< HEAD
-        gtag('config', 'G-T04GQRL8VP');
-=======
         gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
->>>>>>> dev
       `}
       </Script>
       <Script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1" />
