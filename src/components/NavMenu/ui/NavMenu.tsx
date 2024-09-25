478cc795b299f179a501831de19b47b1ec3ab86d
danibosna <daniel.d.boscan@gmail.com>
2024-09-25 01:21:28 -0600

    feat(blog) -> ui blog article cards implementation completed

--- src/components/NavMenu/ui/NavMenu.tsx
+++ src/components/NavMenu/ui/NavMenu.tsx
@@ -1,4 +1,3 @@
-// src/components/NavComponent.tsx
 import React from "react";
 import navigations from "../constants/nav-items";
 import logoIcon from "@/assets/images/danibosna-logo-white.webp";
@@ -18,13 +17,9 @@ const NavComponent: React.FC = () => {
       )}
     >
       <div className="w-full h-full flex flex-col justify-start items-centeri space-y-5">
-        <div className="flex flex-col justify-start text-xs font-bold p-4 md:text-lg">
-          <button
-            id="toggle-button"
-            className="text-center"
-            onClick={() => toggleShowMenu()}
-          >
-            <img src={logoIcon.src} className="w-10" alt="Logo" />
+        <div className="flex flex-col justify-start text-xs font-bold pl-2 pt-2 md:text-lg">
+          <button className="text-center" onClick={() => toggleShowMenu()}>
+            <img src={logoIcon.src} height={45} width={45} alt="Logo" />
           </button>
         </div>
         <div className="nav-item w-full flex flex-col py-2 pl-2">
